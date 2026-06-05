21ebe4ee3093155135518912e3ac57ab55739b19
JuanLif3 <juanitololilifeloli@gmail.com>
2026-06-05 13:01:04 -0400

    fix: Correction in credentials filtred

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -5,21 +5,18 @@ import { ChatParticipant } from '../models/ChatParticipant.model';
 import { Message } from '../models/Message.model';
 import { Invite } from '../models/Invite.model';
 import { Session } from '../models/Session.model';
-import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST || 'ep-lively-unit-ack60rpo-pooler.sa-east-1.aws.neon.tech',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USER || 'neondb_owner',
-    password: process.env.DB_PASSWORD || 'npg_ifovkeM2VZ4A',
-    database: process.env.DB_NAME || 'neondb',
+    host: 'ep-lively-unit-ack60rpo-pooler.sa-east-1.aws.neon.tech',
+    port: 5432,
+    username: 'neondb_owner',
+    password: 'npg_PH4jENX9vAoG',
+    database: 'neondb',
     ssl: {
         rejectUnauthorized: false,
     },
     synchronize: true,
-    logging: process.env.NODE_ENV === 'development',
+    logging: true,
     entities: [User, Chat, ChatParticipant, Message, Invite, Session],
-    subscribers: [],
-    migrations: [],
 });
